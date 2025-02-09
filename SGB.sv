d910bf9ca634eb31aeae4bee7de739b002d629bd
onthewaveagain <62063840+onthewaveagain@users.noreply.github.com>
2025-02-09 17:04:22 +0000

    New LLAPI release

--- SGB.sv
+++ SGB.sv
@@ -301,6 +301,10 @@ wire reset = RESET | buttons[1] | status[0] | cart_download | gb_cart_download |
 `include "build_id.v"
 parameter CONF_STR = {
 	"SGB;SS3F800000:100000;",
+	//LLAPI Always ON
+	"-,>> LLAPI enabled core    <<;",
+	"-,>> Connect USER I/O port <<;",		"-;",
+	//END LLAPI
 	"FS1,GBCGB ;",
 	"OPR,GB Mapper,Auto,WisdomTree,Mani161,MBC1,MBC3;",
 	"-;",
@@ -313,12 +317,6 @@ parameter CONF_STR = {
 	"O[29:28],SGB Speed,SGB1,SGB2,SNES;",
 	"OE,Video Region,NTSC,PAL;",
 	"-;",
-	//LLAPI: OSD menu item
-	//LLAPI Always ON
-	"-,<< LLAPI enabled >>;",
-	"-,<< Use USER I/O port >>;",
-	"-;",
-	//END LLAPI	
 	"C,Cheats;",
 	"H2OO,Cheats Enabled,Yes,No;",
 	"-;",
@@ -1067,6 +1065,17 @@ end
 
 //////////////////   LLAPI   ///////////////////
 
+wire [31:0] llapi_buttons, llapi_buttons2;
+wire [71:0] llapi_analog, llapi_analog2;
+wire [7:0]  llapi_type, llapi_type2;
+wire llapi_en, llapi_en2;
+wire llapi_latch_o, llapi_latch_o2, llapi_data_o, llapi_data_o2;
+wire [12:0] joy_ll_a;
+wire [12:0] joy_ll_b;
+
+//Assign (DOWN + START + FIRST BUTTON) Combinaison to bring the OSD up - P1 and P2 ports.
+wire llapi_osd = (llapi_buttons[26] & llapi_buttons[5] & llapi_buttons[0]) || (llapi_buttons2[26] & llapi_buttons2[5] & llapi_buttons2[0]);
+
 // LLAPI Indexes:
 // 0 = D+    = P1 Latch
 // 1 = D-    = P1 Data
@@ -1075,37 +1084,24 @@ end
 // 4 = RX+   = P2 Latch
 // 5 = RX-   = P2 Data
 
-
-wire [31:0] llapi_buttons, llapi_buttons2;
-wire [71:0] llapi_analog, llapi_analog2;
-wire [7:0]  llapi_type, llapi_type2;
-wire llapi_en, llapi_en2;
-
-wire llapi_select = 1'b1;
-
-wire llapi_latch_o, llapi_latch_o2, llapi_data_o, llapi_data_o2;
-
 always_comb begin
-	USER_OUT= 6'b111111;
-	if (llapi_select) begin
 		USER_OUT[0] = llapi_latch_o;
 		USER_OUT[1] = llapi_data_o;
-		USER_OUT[2] = ~(llapi_select & ~OSD_STATUS); // LED for Blister
+		USER_OUT[2] = OSD_STATUS; // Blister LED
 		USER_OUT[4] = llapi_latch_o2;
 		USER_OUT[5] = llapi_data_o2;
-	end
 end
 
 //Port 1 conf
 LLAPI llapi
 (
 	.CLK_50M(CLK_50M),
-	.LLAPI_SYNC(JOY_STRB),
+	.LLAPI_SYNC(vblank),
 	.IO_LATCH_IN(USER_IN[0]),
 	.IO_LATCH_OUT(llapi_latch_o),
 	.IO_DATA_IN(USER_IN[1]),
 	.IO_DATA_OUT(llapi_data_o),
-	.ENABLE(llapi_select & ~OSD_STATUS),
+	.ENABLE(~OSD_STATUS),
 	.LLAPI_BUTTONS(llapi_buttons),
 	.LLAPI_ANALOG(llapi_analog),
 	.LLAPI_TYPE(llapi_type),
@@ -1116,37 +1112,23 @@ LLAPI llapi
 LLAPI llapi2
 (
 	.CLK_50M(CLK_50M),
-	.LLAPI_SYNC(JOY_STRB),
+	.LLAPI_SYNC(vblank),
 	.IO_LATCH_IN(USER_IN[4]),
 	.IO_LATCH_OUT(llapi_latch_o2),
 	.IO_DATA_IN(USER_IN[5]),
 	.IO_DATA_OUT(llapi_data_o2),
-	.ENABLE(llapi_select & ~OSD_STATUS),
+	.ENABLE(~OSD_STATUS),
 	.LLAPI_BUTTONS(llapi_buttons2),
 	.LLAPI_ANALOG(llapi_analog2),
 	.LLAPI_TYPE(llapi_type2),
 	.LLAPI_EN(llapi_en2)
 );
 
-reg llapi_button_pressed, llapi_button_pressed2;
-
-always @(posedge CLK_50M) begin
-	if (reset) begin
-		llapi_button_pressed  <= 0;
-		llapi_button_pressed2 <= 0;
-	end else begin
-		if (|llapi_buttons)
-			llapi_button_pressed  <= 1;
-		if (|llapi_buttons2)
-			llapi_button_pressed2 <= 1;
-	end
-end
-
 // controller id is 0 if there is either an Atari controller or no controller
-// if id is 0, assume there is no controller until a button is pressed
-// also check for 255 and treat that as 'no controller' as well
-wire use_llapi  = llapi_en  && llapi_select && ((|llapi_type  && ~(&llapi_type))  || llapi_button_pressed);
-wire use_llapi2 = llapi_en2 && llapi_select && ((|llapi_type2 && ~(&llapi_type2)) || llapi_button_pressed2);
+// if id is 0, assume there is no controller
+// also check for 255 ('Searching mode') and treat that as 'no controller' as well
+wire use_llapi  = llapi_en && ((|llapi_type  && ~(&llapi_type))); //  || llapi_button_pressed);
+wire use_llapi2 = llapi_en2 && ((|llapi_type2 && ~(&llapi_type2))); // || llapi_button_pressed2);
 
 //Controller string provided by core for reference (order is important)
 //Controller specific mapping based on type. More info here : https://docs.google.com/document/d/12XpxrmKYx_jgfEPyw-O2zex1kTQZZ-NSBdLO2RQPRzM/edit
@@ -1154,7 +1136,6 @@ wire use_llapi2 = llapi_en2 && llapi_select && ((|llapi_type2 && ~(&llapi_type2)
 
 //Port 1 mapping
 
-wire [12:0] joy_ll_a;
 always_comb begin
 	// map for saturn controller
 	// use L and R instead of top face buttons
@@ -1178,7 +1159,6 @@ end
 
 //Port 2 mapping
 
-wire [12:0] joy_ll_b;
 always_comb begin
 	// map for saturn controller
 	// use L and R instead of top face buttons
@@ -1200,35 +1180,23 @@ always_comb begin
 	end
 end
 
-//Assign (DOWN + START + FIRST BUTTON) Combinaison to bring the OSD up - P1 and P2 ports.
-
-wire llapi_osd = (llapi_buttons[26] && llapi_buttons[5] && llapi_buttons[0]) || (llapi_buttons2[26] && llapi_buttons2[5] && llapi_buttons2[0]);
-
-// if LLAPI is enabled, shift USB controllers to next available player slot
+// Player / LLAPI port allocation
 always_comb begin
-	 if (use_llapi & use_llapi2) begin
+        if (~use_llapi & use_llapi2)  begin
+               	joy0 = joy_ll_b;
+                joy1 = joy_usb_0;
+                joy2 = joy_usb_1;
+                joy3 = joy_usb_2;
+                joy4 = joy_usb_3;
+        end else begin
                 joy0 = joy_ll_a;
                 joy1 = joy_ll_b;
                 joy2 = joy_usb_0;
                 joy3 = joy_usb_1;
                 joy4 = joy_usb_2;
-        end else if (use_llapi ^ use_llapi2) begin
-                joy0 = use_llapi  ? joy_ll_a : joy_usb_0;
-                joy1 = use_llapi2 ? joy_ll_b : joy_usb_0;
-                joy2 = joy_usb_1;
-                joy3 = joy_usb_2;
-                joy4 = joy_usb_3;
-        end else begin
-                joy0 = joy_usb_0;
-                joy1 = joy_usb_1;
-                joy2 = joy_usb_2;
-                joy3 = joy_usb_3;
-                joy4 = joy_usb_4;
-        end
+		end
 end
 
-
-//////////////////   END LLAPI   ///////////////////												
 
 /////////////////////////  GB SAVE/LOAD  /////////////////////////////
 wire [7:0] ram_mask_file;
